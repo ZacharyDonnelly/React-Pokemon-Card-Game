9819dab067f6c42bb71ae6b1f2e450afd8c3db1f
Hashed <z-donnelly@hvcc.edu>
2019-11-04 22:20:17 -0500

    Finish logic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import Pokecard from './Pokecard'
+import Pokedex from './Pokedex'
 import './App.css';
 
 function App() {
   return ( 
   <div className="App">
-  <Pokecard 
-    id={4}
-    name='Charmander'
-    type='fire'
-    exp={62}
-  />
+  <Pokedex />
   </div>
   )
 }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import './Pokecard.css'
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+import './Pokecard.css';
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 class Pokecard extends Component {
     render(){
