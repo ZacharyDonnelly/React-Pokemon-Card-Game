74dc6b1cdff3bdaadcefd542cd3546f7de544b6c
Hashed <z-donnelly@hvcc.edu>
2019-11-04 22:04:53 -0500

    Initial commit

---
+++ src/Pokecard.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './Pokecard.css'
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+class Pokecard extends Component {
+    render(){
+        let imgSrc = `${POKE_API}${this.props.id}.png`
+        return(
+            <div className='Pokecard'>
+                <h1>{this.props.name}</h1>
+                <img src={imgSrc} alt={this.props.name}/>
+                <div>Type: {this.props.type}</div>
+                <div>EXP: {this.props.exp}</div>
+            </div>
+        )
+    }
+}
+
+export default Pokecard
